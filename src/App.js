17de5039c140de992b74f241ab502c6fe6e70a3e
ronatio <ronatioparhorasansilaban8@gmail.com>
2022-09-20 09:56:46 +0700

    React-2-JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 
 import './App.css';
+import Navbar from "./components/nav"
 
-function App() {
+function App({name, job}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+    <div>    
         <h1>Bootcamp WGS Batch 3</h1>
-        <p>Ronatio Parhorasan Silaban</p>
-      </header>
-    </div>
+        <p>Hallo Saya{name}, Saya adalah {job} Bootcamp Wgs Batch 3</p>
+    
+        </div>
   );
 }
 
---
+++ src/pertemuan2.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navigasi from "./components/nav";
+import Container from 'react-bootstrap/Container';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
+import App from "./App"
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App name={"ronatio"} job={"peserta"}/>
+    <App name={"Dani"} job={"peserta"}/>
+  </React.StrictMode>
+);
+
+// function renderDOM(content, id) {
+//   ReactDOM.render(content, document.getElementById(id))
+  
+// }
+
+// const element = <div style={{color:"black"}}>
+//   <Navigasi/>
+//   <Container className='Container'>
+//   <h1>Bootcamp WGS Batch 3</h1>
+//   <h2>Ronatio Parhorasan Silaban</h2>
+//   </Container>
+//   </div>;
+
+
+// renderDOM(element,"root")
