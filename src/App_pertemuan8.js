6931bdbde6756a3fd1580fcd995315d6544f0a14
ronatio <ronatioparhorasansilaban8@gmail.com>
2022-09-29 16:44:33 +0700

    react-ref

--- src/App_pertemuan8.js
+++ src/App_pertemuan8.js
@@ -2,8 +2,7 @@ import './App.css';
 import Image from './components/image';
 import React from 'react';
 
-//menggannti function component menjadi class component
-//pemanggilan data menjadi this.prop.data
+//react ref
 function App() {
   return(
     <div>
