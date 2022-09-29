28dcb917c032efc18377c2be559d076c029aab94
ronatio <ronatioparhorasansilaban8@gmail.com>
2022-09-29 16:43:03 +0700

    from

--- src/App_pertemuan7.js
+++ src/App_pertemuan7.js
@@ -2,8 +2,6 @@ import './App.css';
 import Form from './components/form';
 import React from 'react';
 
-//menggannti function component menjadi class component
-//pemanggilan data menjadi this.prop.data
 function App() {
   return(
     <div>
